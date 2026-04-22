d90cb91884930f08bdc82fa6478dfa71ec5c4616
anshikakalhans <anshikaakalhans@gmail.com>
2026-04-22 22:09:08 +0530

    feat:input code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        //output
+//        System.out.println("Hello World");
+//    }
+//}
+
+
+//Q. Print star pattern.
+ // *
+ // **
+ // ***
+ // ****
+ // *****
+
+//public class Main{
+//    public static void main(String[] args){
+//        //output
+//        System.out.println("*");
+//        System.out.println("**");
+//        System.out.println("***");
+//        System.out.println("****");
+//        System.out.println("*****");
+//    }
+//}
+
+
+// Taking input
+
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        //output
-        System.out.println("Hello World");
+    public static void main(String[]args) {
+        // Input
+        Scanner src = new Scanner(System.in);
+        String name = src.nextLine();
+        //nextInt()
+        //nextFloat()
+        System.out.println(name);
     }
 }
+
+
+
