31f18e7dca1b8fca550453271433026b6faf88e9
anshikakalhans <anshikaakalhans@gmail.com>
2026-05-07 23:11:26 +0530

    Initial commit

---
+++ Conditions.java
@@ -0,0 +1,125 @@
+// public class Main {
+// public static void main(String[] args) {
+// //output
+// System.out.println("Hello World");
+// }
+// }
+
+// Q. Print star pattern.
+// *
+// **
+// ***
+// ****
+// *****
+
+// public class Main{
+// public static void main(String[] args){
+// //output
+// System.out.println("*");
+// System.out.println("**");
+// System.out.println("***");
+// System.out.println("****");
+// System.out.println("*****");
+// }
+// }
+
+// Taking input
+
+// import java.util.*;
+
+// public class Main {
+// public static void main(String[]args) {
+// // Input
+// Scanner src = new Scanner(System.in);
+// String name = src.nextLine();
+// //nextInt()
+// //nextFloat()
+// System.out.println(name);
+// }
+// }
+
+// Conditional Statements
+// if-else
+
+// import java.util.*;
+
+// public class Conditions {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+// int age = sc.nextInt();
+
+// if (age > 18) {
+// System.out.println("Adult");
+
+// } else {
+// System.out.println("Not Adult");
+// }
+// }
+// }
+
+// checking odd-even
+
+// import java.util.*;
+
+// public class Conditions {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+// int x = sc.nextInt();
+
+// if (x % 2 == 0) {
+// System.out.println("Even");
+
+// } else {
+// System.out.println("Odd");
+// }
+// }
+// }
+
+// ----- Comparing two numbers -----
+// import java.util.*;
+
+// public class Conditions {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+// if (a == b) {
+// System.out.println("a is equal to b");
+// } else if (a > b) {
+// System.out.println("a is greater than b");
+
+// } else {
+// System.out.println("a is less than b");
+// }
+// }
+// }
+
+// Q. Print the Greeting when buttons are pressed -
+// button 1 - Hello
+// button 2 - namaste
+// button 3 - bonjour
+
+// import java.util.*;
+
+// public class Conditions {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in); // creates a scanner object named scthat
+// reads input from keyboard(System.in)
+
+// int button = sc.nextInt();
+
+// switch (button) {
+// case 1:
+// System.out.println("hello");
+// break;
+// case 2:
+// System.out.println("namaste");
+// break;
+// case 3:
+// System.out.println("bonjour");
+// break;
+// default:
+// System.out.println("invalid button");
+// }
+// }
+// }
