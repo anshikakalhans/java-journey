8dbed67a59e59c3490c23ff2013700fa49c57526
anshikakalhans <anshikaakalhans@gmail.com>
2026-06-07 01:31:47 +0530

    feat: loops

---
+++ Loops.java
@@ -0,0 +1,61 @@
+// ----- for loop -----
+
+// public class Loops {
+// public static void main(String args[]) {
+// for(int counter = 0; counter < 5; counter = counter+1){
+// System.out.println("hello world");
+// }
+// }
+// }
+
+// Print numbers from 1 to 10
+
+// public class Loops{
+// public static void main(String args[]) {
+// for(int counter = 0; counter < 6; counter++){
+// System.out.println(counter);
+// }
+// }
+// }
+
+// ----- while loop -----
+
+// public class Loops{
+// public static void main(String args[]) {
+// int i = 0;
+// while(i < 11){
+// System.out.println(i);
+// i = i + 1;
+// }
+// }
+// }
+
+// ----- do-while loop -----
+
+// public class Loops{
+// public static void main(String args[]){
+// int i = 0;
+// do {
+// System.out.println(i);
+// i = i + 1;
+// } while (i < 6);
+// }
+// }
+
+// print sum of first n natural numbers.
+
+// import java.util.*;
+
+// public class Loops {
+// public static void main(String args[]) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+
+// int sum = 0;
+// for (int i = 0; i <= n; i++) {
+// sum = sum + i;
+// }
+// System.out.println(sum);
+// }
+
+// }
